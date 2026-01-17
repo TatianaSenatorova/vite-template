552faacfd6f4284ac3e280eac95cd00df8c21b92
TatianaSenatorova <1975tanya@bk.ru>
2026-01-17 17:23:10 +0300

    fix: убираем проверку на наличие link css

--- .linthtmlrc.cjs
+++ .linthtmlrc.cjs
@@ -1,6 +1,8 @@
 module.exports = {
-  extends: ["linthtml-config-htmlacademy"],
+  extends: ['linthtml-config-htmlacademy'],
   rules: {
     'htmlacademy/no-blocking-script': false,
+    'htmlacademy/req-stylesheet-link': false,
   },
 };
+
